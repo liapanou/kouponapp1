b4c3066bd6edf3ca077ea5dd09c79d834bd9166e
liapanou7 <liapanou7@gmail.com>
2022-07-01 12:26:45 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+        Responsive
+      </button>
+    </div>
+  );
+}
+
+export default Home;

cfc674d1bfcd3bd631811880c5d5692ca9a7309a
liapanou7 <liapanou7@gmail.com>
2022-07-16 17:46:49 +0300

    new card

--- components/cardmagazia/index.tsx
+++ components/cardmagazia/index.tsx
@@ -1,18 +1,25 @@
 export function CardMagazia(props) {
   return (
-    <div>
-      <div className="card w-full bg-base-300 shadow-xl border border-base-100 ">
-        <figure className="px-10 pt-10">
-          <img
-            src="http://dummyimage.com/250x250.png/cc0000/ffffff"
-            alt="Shoes"
-            className="rounded-xl w-full h-28"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title text-xs h-10">{props.name}</h2>
-          <div className="card-actions">
-            <button className="btn badge ">{props.points}points Buy Now</button>
+    <div className=" px-4 py-6">
+      <div className=" container mx-auto relative ">
+        <div className="bg-white absolute right-1/4 box-border h-20 w-20 p-4 ml-auto z-20 ">
+          200 points
+        </div>
+        <img
+          className="rounded-lg shadow-md h-64 relative z-10"
+          src="https://www.panelinios.gr/images/photo_big/104871.jpg"
+          alt=""
+        ></img>
+      </div>
+      <div className="px-4 ">
+        <div className=" bg-white  relative rounded-lg px-4 py-3 w-80 -mt-16 z-20">
+          <h4 className="font-semibold">Deseo art cafe</h4>
+          <div className="flex">
+            <div className=" text-xs font-medium">Coffees</div>
+            <br />
+            <div className="text-xs text-gray-600 font-medium ">
+              &middot; 30' &middot; min 0,00€
+            </div>
           </div>
         </div>
       </div>

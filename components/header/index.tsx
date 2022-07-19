f8e30d91fafa8f642384886d6792973f703db13c
liapanou7 <liapanou7@gmail.com>
2022-07-19 18:31:25 +0300

    new header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "../../node_modules/next/link";
 import { Input } from "../input";
 
@@ -5,35 +6,15 @@ export function Header() {
   return (
     <div className="navbar sticky top-0 shadow z-50 bg-base-100 ">
       <div className="flex-1">
+        <button>
+          <img src="https://s2.svgbox.net/octicons.svg?ic=filter" alt=""></img>
+        </button>
+
         <Link href={"/"}>
           <a className="btn btn-ghost normal-case text-xl">KouponApp</a>
         </Link>
       </div>
-      <div className="flex flex-1  items-center justify-center">
-        <div className="input-group ">
-          <input
-            type="text"
-            placeholder="Search…"
-            className="input input-bordered"
-          />
-          <button className="btn btn-square">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+      <div className="flex flex-1  items-center justify-center"></div>
 
       <div className="flex-none gap-2">
         <div className="dropdown dropdown-end">
